815aeec1bacd28718f0267a6af62b3ad9e5cca41
nikolovsk <129659501+nikolovsk@users.noreply.github.com>
2024-12-18 15:30:13 +0100

    Delete src directory

--- src/main/java/mk/finki/ukim/mk/lab/model/exceptions/PasswordsDoNotMatchException.java
+++
@@ -1,7 +0,0 @@
-package mk.finki.ukim.mk.lab.model.exceptions;
-
-public class PasswordsDoNotMatchException extends RuntimeException{
-    public PasswordsDoNotMatchException() {
-        super("Passwords do not match exception");
-    }
-}
